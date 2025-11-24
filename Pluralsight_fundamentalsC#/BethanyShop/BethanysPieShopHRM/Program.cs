07ca9243fec67c9b4cb7d59d16378141045e15e0
Mariyah-ACN <mariyah.sajjad@accenture.com>
2025-11-24 17:01:47 +0000

    Adding Pluralsight_fundamentalsC# folder containing the HelloWorld app and the beginning of Bethanysshop app

---
+++ Pluralsight_fundamentalsC#/BethanyShop/BethanysPieShopHRM/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to Bethany's Pie Shop HRM!");
+
+Console.WriteLine("Please enter your name:");
+
+
+string name = Console.ReadLine();
+
+int monthlyWage = 1234;
+int months = 12, bonus = 1000; 
+
+bool isActive = true;
+
+double rating = 99.25;
+
+//byte numberOfEmployees = 300;
+
+int hoursWorked;
+hoursWorked = 125;
+hoursWorked = 148; 
+
+const double interestRate = 0.07;
+
+//interestRate = 0.08;
+
+string firstName = "Bethany";
+string lastName = "Smith";
+string emptyString = "";
+
+
+
